c659614d8ae55f5d36d324ef20468942567ae329
Alex Sweeney <a.sweeney@ryte.com>
2024-04-04 19:36:41 +0100

    add Signup button

---
+++ src/components/atoms/SignupButton/SignupButton.tsx
@@ -0,0 +1,70 @@
+import colors from '../../../theme/colors';
+import GoogleLogo from '../../../icons/GoogleLogo';
+import GitHubLogo from '../../../icons/GitHubLogo';
+import EmailIcon from '../../../icons/EmailIcon';
+
+interface SignupButtonProps {
+  text: String;
+  color: 'black'|'green',
+  active: boolean,
+  icon: 'google'|'github'|'email',
+}
+
+const SignupButton = ({
+  text,
+  color,
+  active,
+  icon
+}: SignupButtonProps) => { 
+  const backgroundColors = {
+    'black': colors.black,
+    'green': colors.green,
+  };
+
+  const icons = {
+    'google': GoogleLogo,
+    'github': GitHubLogo,
+    'email': EmailIcon
+  };
+
+  const Icon = icons[icon];
+
+  const buttonStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '360px',
+    height: '56px',
+    opacity: active ? 1 : 0.6,
+    background: backgroundColors[color],
+    border: 'none',
+    color: colors.white,
+    fontFamily: 'Inter',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    borderRadius: '4px'
+  };
+
+  const iconContainer = {
+    width: '24px',
+    height: '24px',
+    marginRight: '8px'
+  };
+
+  const textContainer = {
+    marginLeft: '8px'
+  }
+
+  return (
+    <button style={buttonStyle}>
+      <div style={iconContainer}>
+        <Icon/>
+      </div>
+      <div style={textContainer}>
+        {text}
+      </div> 
+    </button>
+  )
+};
+
+export default SignupButton;
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,6 +1,7 @@
 const colors = {
   'green': '#02B159',
   'white': '#FEFEFE',
+  'black': '#242424',
 };
 
 export default colors;

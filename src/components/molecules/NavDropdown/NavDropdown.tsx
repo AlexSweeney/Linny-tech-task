3f1573b8dfff937d7b921a0f164df372230243fd
Alex Sweeney <a.sweeney@ryte.com>
2024-04-05 15:19:39 +0100

    add NavDropdown component

---
+++ src/components/molecules/NavDropdown/NavDropdown.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import colors from '../../../theme/colors';
+
+interface NavDropdown {
+  items: string[]
+}
+
+const NavDropdown = ({
+  items
+}: NavDropdown) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuIconStyle = {
+    color: colors.grey,
+    width: '50px',
+    height: '50px',
+  };
+
+  return (
+    <div>
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon style={menuIconStyle}/>
+      </IconButton>
+      
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {
+          items.map(item =><MenuItem onClick={handleClose}>{item}</MenuItem> )
+        }
+      </Menu>
+    </div>
+  );
+}
+
+export default NavDropdown;

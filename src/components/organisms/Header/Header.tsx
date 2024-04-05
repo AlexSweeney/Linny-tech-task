bb39934c986637c87e6ef1dcf44e3416809e5227
Alex Sweeney <a.sweeney@ryte.com>
2024-04-05 16:01:51 +0100

    add Header component

---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,49 @@
+import colors from "../../../theme/colors";
+import NavButton from "../../atoms/NavButton/NavButton";
+import NavDropdown from "../../molecules/NavDropdown/NavDropdown";
+import NavBar from "../../molecules/NavBar/NavBar";
+
+const Header = () => {
+  const navContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: '92px',
+    width: '100%',
+    background: colors.black,
+    color: colors.grey,
+    padding: '0 32px',
+    'box-sizing': 'border-box',
+  };
+
+  const logoStyle = {
+    fontSize: '36px',
+    fontFamily: 'Inter',
+    fontWeight: 'normal',
+  };
+
+  const navBarItems = [
+    'Feature',
+    'Edge Network',
+    'Pricing',
+    <NavButton text="Sign Up"/>
+  ];
+
+  const navDropdownItems = [
+    'Feature',
+    'Edge Network',
+    'Pricing',
+    'Sign Up',
+  ];
+
+  return (
+    <div style={navContainerStyle}>
+      <h1 style={logoStyle}>SuperNet</h1>
+
+      <NavBar items={navBarItems}/>
+      <NavDropdown items={navDropdownItems}/>
+    </div>
+  );
+}
+
+export default Header;

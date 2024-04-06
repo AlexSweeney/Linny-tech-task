fd81b59c76455f7df38b31f9517339da10ae70d9
Alex Sweeney <a.sweeney@ryte.com>
2024-04-06 14:31:53 +0100

    add FeaturesCard component

---
+++ src/components/molecules/FeaturesCard/FeaturesCard.tsx
@@ -0,0 +1,72 @@
+import { ReactElement } from "react";
+import colors from "../../../theme/colors";
+import './FeaturesCard.css';
+
+interface FeaturesCardProps {
+  image: ReactElement,
+  children: ReactElement|ReactElement[],
+}
+
+const FeaturesCard = ({
+  image,
+  children
+}: FeaturesCardProps) => {
+  const containerStyle = {
+    display: 'flex',
+    position: 'relative' as 'relative',
+    maxWidth: '630px',
+    minHeight: '325px',
+    background: colors.grey,
+    'box-shadow': '0 16px 16px rgba(0,0,0,0.1)',
+    overflow: 'hidden',
+  };
+
+  const textContainerStyle = {
+    position: 'absolute' as 'absolute',
+    width: '100%',
+    minHeight: '100%',
+    'z-index': '1000',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    'flex-direction': 'column',
+  };
+
+  const textWrapperStyle = {
+    maxWidth: '315px',
+    color: colors.lightGrey,
+    fontFamily: 'Inter',
+    padding: '24px',
+    display: 'flex',
+    'flex-direction': 'column',
+  };  
+
+  const imageContainerStyle = {
+    position: 'absolute' as 'absolute',
+    width: '100%',
+    height: '100%',
+    'z-index': '0',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  }
+
+  const imageWrapperStyle = {
+    maxWidth: '315px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={textContainerStyle}>
+        <div style={textWrapperStyle}>
+          {children}
+        </div>
+      </div>
+      <div style={imageContainerStyle}>
+        <div style={imageWrapperStyle}>
+          {image}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturesCard;

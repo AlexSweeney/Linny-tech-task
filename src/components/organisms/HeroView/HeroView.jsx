a3f81f56e63fd63b419e004331df5e68aa6ad4ff
Alex Sweeney <a.sweeney@ryte.com>
2024-04-07 13:26:18 +0100

    add mobile styles for HeroView

--- src/components/organisms/HeroView/HeroView.jsx
+++ src/components/organisms/HeroView/HeroView.jsx
@@ -1,4 +1,5 @@
 import './../../../theme/text.css';
+import './HeroView.css';
 import SignupForm from "../../molecules/SignupForm/SignupForm";
 
 const HeroView = () => {
@@ -6,7 +7,6 @@ const HeroView = () => {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
     'box-sizing': 'border-box',
     padding: '32px 10px',
     width: '100%',
@@ -24,14 +24,11 @@ const HeroView = () => {
   const heroTextWrapper = {
     display: 'flex',
     'flex-direction': 'column',
-    justifyContent: 'center',
     width: '100%',
     maxWidth: '525px',
-    height: '460px',
   };
 
   const heroHeadingStyle = {
-    fontSize: '44px',
     marginTop: '0',
     marginBottom: '0',
   };
@@ -41,10 +38,10 @@ const HeroView = () => {
   };
 
   return (
-    <div style={heroContainer}>
+    <div style={heroContainer} className='heroContainer'>
       <div style={heroWrapper}>
-          <div style={heroTextWrapper}>
-          <h1 style={heroHeadingStyle}>Cloud Computing Developers Trust</h1>
+        <div style={heroTextWrapper} className='heroTextWrapper'>
+          <h2 style={heroHeadingStyle}>Cloud Computing Developers Trust</h2>
           <p style={heroTextStyle}>Build, run, and secure your cloud workloads on <span className="bold">SuperNet Connected Cloud</span>, a massively distributed edge and cloud platform. <span className="highlight">Sign up</span> today or <span className="highlight">contact us</span> to learn more.</p>
         </div>
         <SignupForm/>
